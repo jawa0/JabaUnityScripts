2ee76ce8b7c9ee78111e54193b4aa9dacab7f9eb
Jabavu Adams <jaba.adams@gmail.com>
2015-11-22 14:06:01 -0500

    Added script that draws all of an object's collision contact points (normals).

---
+++ Assets/JabaScripts/DebugDrawCollisions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DebugDrawCollisions : MonoBehaviour 
+{
+	public float normalScale = 1.0f;
+	public Color contactNormalColor = Color.yellow;
+
+	void OnCollisionStay( Collision collisionInfo )
+	{
+		foreach ( ContactPoint cp in collisionInfo.contacts )
+		{
+			Debug.DrawRay( cp.point, normalScale * cp.normal, contactNormalColor, 0.0f, false );
+		}
+	}
+}
