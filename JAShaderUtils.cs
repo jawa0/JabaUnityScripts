95b46c8b6498e2b00661305eb98ad4c8e1ecd21d
Jabavu Adams <jaba.adams@gmail.com>
2016-03-28 19:36:58 -0400

    Added some utility code that can set the Standard Shader's rendering mode from script code.

---
+++ JAShaderUtils.cs
@@ -0,0 +1,64 @@
+﻿// This is a modified version of some code from the Unity forums, here:
+// http://forum.unity3d.com/threads/standard-material-shader-ignoring-setfloat-property-_mode.344557/
+
+using UnityEngine;
+
+public class JAShaderUtils : MonoBehaviour {
+	public enum StandardShaderRenderingMode
+	{
+		Opaque,
+		Cutout,
+		Fade,        // Old school alpha-blending mode, fresnel does not affect amount of transparency
+		Transparent // Physically plausible transparency mode, implemented as alpha pre-multiply
+	}
+	
+	public static void SetMaterialRenderingMode(Material material, StandardShaderRenderingMode mode)
+	{
+		switch (mode)
+		{
+		case StandardShaderRenderingMode.Opaque:
+			material.SetOverrideTag("RenderType", "");
+			material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+			material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+			material.SetInt("_ZWrite", 1);
+			material.DisableKeyword("_ALPHATEST_ON");
+			material.DisableKeyword("_ALPHABLEND_ON");
+			material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+			material.renderQueue = -1;
+			break;
+			
+		case StandardShaderRenderingMode.Cutout:
+			material.SetOverrideTag("RenderType", "TransparentCutout");
+			material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+			material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+			material.SetInt("_ZWrite", 1);
+			material.EnableKeyword("_ALPHATEST_ON");
+			material.DisableKeyword("_ALPHABLEND_ON");
+			material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+			material.renderQueue = 2450;
+			break;
+			
+		case StandardShaderRenderingMode.Fade:
+			material.SetOverrideTag("RenderType", "Transparent");
+			material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+			material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			material.SetInt("_ZWrite", 0);
+			material.DisableKeyword("_ALPHATEST_ON");
+			material.EnableKeyword("_ALPHABLEND_ON");
+			material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+			material.renderQueue = 3000;
+			break;
+			
+		case StandardShaderRenderingMode.Transparent:
+			material.SetOverrideTag("RenderType", "Transparent");
+			material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+			material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			material.SetInt("_ZWrite", 0);
+			material.DisableKeyword("_ALPHATEST_ON");
+			material.DisableKeyword("_ALPHABLEND_ON");
+			material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+			material.renderQueue = 3000;
+			break;
+		}
+	}
+}
